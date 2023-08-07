2ec5764cc316a35cee5ccef19f83d0030fed4278
Abdul wahab shaikh <130089519+wahabph1@users.noreply.github.com>
2023-08-07 19:08:43 +0500

    program to create linked list

---
+++ 03 node.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class A {
+	public:
+	int data ;
+	A *next;
+	A(int d)
+	{
+		data = d;
+	}
+};
+int main ()
+{
+	A*node1 = new A(3);
+	A*node2 = new A(4);
+	A*node3 = new A(5);
+	A*node4 = new A(6);
+	
+	A *head = node1;
+	head->next = node2;
+	node2->next= node3;
+	node3->next = node4;
+	node4->next = NULL;
+    cout<<node2->next;
+
+
+
+}
+	
+	
+	
+	 
+	
+
